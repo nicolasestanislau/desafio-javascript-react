2a41c9bfa298618ede53b767efbebc3c536ff85d
nicolasestanislau <nicolas_estanislau@hotmail.com>
2022-08-31 22:38:33 -0300

    modified

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import HeaderBar from "./components/headerBar";
+import "@fontsource/open-sans";
+import './assets/css/style.scss'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <HeaderBar />
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
